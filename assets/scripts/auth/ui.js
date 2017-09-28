1ed4d04626bce1673c7a35f57f3aa217de483311
hilarybrown <hilary.brown@libertymutual.com>
2017-09-28 16:03:12 -0400

    removed board refresh on sign out because it's throwing errors

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -54,7 +54,6 @@ const signOutSuccess = function (data) {
   console.log('Signed out successfully')
   $('#user-action-message').text('Come back again soon!')
   store.user = null
-  $('#game-board')[0].reset()
   $('#sign-in')[0].reset()
   $('#sign-up')[0].reset()
   $('#sign-in-message').show()
