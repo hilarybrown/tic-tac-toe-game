41af1ea6402e6d0e8d22eba674fc3cd18903c2c7
hilarybrown <hilary.brown@libertymutual.com>
2017-09-28 14:13:13 -0400

    Updates to user api
    hid board, added sign in messages, hid and un-hid fields based on
    signing in and out, cleared input fields upon successful button action

--- scripts/sign-in.sh
+++ scripts/sign-in.sh
@@ -1,7 +1,10 @@
 # API="${API_ORIGIN:-http://localhost:7165}"
 # URL_PATH="/sign-in"
-API="${API_ORIGIN:-https://hilarybrown.github.io/tic-tac-toe-game}"
+API="${API_ORIGIN:-http://tic-tac-toe.wdibos.com}"
 URL_PATH="/sign-in"
+# API="${API_ORIGIN:-https://aqueous-atoll-85096.herokuapp.com/}"
+# URL_PATH="/sign-in"
+
 
 curl "${API}${URL_PATH}" \
   --include \
--- scripts/sign-out.sh
+++ scripts/sign-out.sh
@@ -1,7 +1,9 @@
 # API="${API_ORIGIN:-http://httpbin.org}"
 # URL_PATH="/delete?id=$ID"
-API="${API_ORIGIN:-https://hilarybrown.github.io/tic-tac-toe-game}"
+API="${API_ORIGIN:-http://tic-tac-toe.wdibos.com}"
 URL_PATH="/sign-out/$ID"
+# API="${API_ORIGIN:-https://aqueous-atoll-85096.herokuapp.com/}"
+# URL_PATH="/sign-out/$ID"
 
 curl "${API}${URL_PATH}" \
   --include \
